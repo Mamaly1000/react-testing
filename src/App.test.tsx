0e0abdaaa5d50197453288029c283b774ed5b268
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-07-23 13:58:18 +0330

    learned about grouping tests and also filtering them through the terminal

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,19 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import Greet from "./greet";
 
-test("testing the greeting", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("*app*", () => {
+  test("testing the app", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
-test("testing the greeting with name", () => {
-  const name = "mamaly";
-  render(<Greet name={name} />);
-  const GreetText = screen.getByText(`hello to you ${name}`);
-  expect(GreetText).toBeInTheDocument();
+describe("*greet*", () => {
+  test("testing the greeting with name", () => {
+    const name = "mamaly";
+    render(<Greet name={name} />);
+    const GreetText = screen.getByText(`hello to you ${name}`);
+    expect(GreetText).toBeInTheDocument();
+  });
 });
